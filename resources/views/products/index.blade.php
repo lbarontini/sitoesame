fe0cfcf788603e21d4f0698c56555dc869913e11
LORENZO BARONTINI <S1080199@studenti.univpm.it>
2021-07-30 14:49:43 +0200

    updated products layout

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,42 +4,20 @@
 <div id="maincontent" class="bodywidth clear">
     <section id="tools">
         <a href="/products/create">aggiungi</a>
-    <section id="aboutleft">
+    </section>
+    <section id="product">
         @foreach ($products as $product)
             <article>
-                <div>
-                    <figure> <img src="/images/articleimage.jpg" alt=""> </figure>
-                    {{-- <figure> <img src={{$product->photo_path}} alt=""> </figure> --}}
-                    <header><a href="/products/{{$product->id}}"><h3>{{$product->model}}</h3></a></header>
-                    <h4>{{$product->description}}</h4>
-                    <h5>{{$product->installation_notes}}</h5>
-                    <h5>{{$product->use_notes}}</h5>
-                </div>
+                <figure> <img src="/images/articleimage.jpg" alt=""> </figure>
+                {{-- <figure> <img src={{$product->photo_path}} alt=""> </figure> --}}
+                <a href="/products/{{$product->id}}">
+                    <h3 class = "blue">{{$product->model}}</h3>
+                </a>
+                <h4 >{{$product->description}}</h4>
+                <h5>{{$product->installation_notes}}</h5>
+                <h5>{{$product->use_notes}}</h5>
             </article>
         @endforeach
     </section>
-    {{-- <section id="articlesright">
-        <article>
-            <figure> <img src="/images/articleimage.jpg" alt=""> </figure>
-            <header><a href="#">
-            <h5>Random News Article</h5>
-            </a></header>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis molestie sapien. Proin elit quam dolor amet...</p>
-            <a href="#" class="readmore">Read more</a> </article>
-        <article>
-            <figure> <img src="/images/articleimage.jpg" alt=""> </figure>
-            <header><a href="#">
-            <h5>Random News Article</h5>
-            </a></header>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis molestie sapien. Proin elit quam dolor amet...</p>
-            <a href="#" class="readmore">Read more</a> </article>
-        <article>
-            <figure> <img src="/images/articleimage.jpg" alt=""> </figure>
-            <header><a href="#">
-            <h5>Random News Article</h5>
-            </a></header>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis molestie sapien. Proin elit quam dolor amet...</p>
-            <a href="#" class="readmore">Read more</a> </article>
-    </section> --}}
 </div>
 @endsection
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -5,12 +5,13 @@
     <section id="tools">
         <a href="{{$product->id}}/edit">Modifica</a>
         {{-- <a href="delete">Elimina</a> --}}
-    <section id="aboutleft">
+    </section>
+    <section id="product">
         <article>
             <div>
                 <figure> <img src="/images/articleimage.jpg" alt=""> </figure>
                 {{-- <figure> <img src={{$product->photo_path}} alt=""> </figure> --}}
-                <h3>{{$product->model}}</h3>
+                <h3 class="blue">{{$product->model}}</h3>
                 <h4>{{$product->description}}</h4>
                 <h5>{{$product->installation_notes}}</h5>
                 <h5>{{$product->use_notes}}</h5>
